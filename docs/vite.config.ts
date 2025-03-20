6e1be3a4521b23c74268c75b80c0597da222d33d
ChapoLapa <nikkihochmuth@gmail.com>
2025-03-20 22:27:37 +0100

    Update vite.config.ts

--- docs/vite.config.ts
+++ docs/vite.config.ts
@@ -1,11 +1,8 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'useaichatbots',  // WICHTIG für GitHub Pages!
-  optimizeDeps: {
-    exclude: ['lucide-react'],
-  },
+  base: '/useaichatbots/',  // Hier den Namen deines Repositorys einfügen
 });
+
